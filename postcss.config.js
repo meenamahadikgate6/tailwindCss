83615e335d9e85f9b9a6528f2389bce05c373ac6
meenamahadikgate6 <meena.mahadik@gate6.com>
2025-03-13 16:16:17 +0530

    run project first time

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,6 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-
-
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
-};
-
-
-// module.exports = {
-//   syntax: 'postcss-scss',
-//   plugins: [
-//       'postcss-sassy-mixins',
-//       'postcss-import',
-//       'postcss-flexbugs-fixes',
-//       'postcss-advanced-variables',
-//       [
-//           'postcss-preset-env',
-//           {
-//               autoprefixer: {
-//                   flexbox: 'no-2009',
-//               },
-//               stage: 3,
-//               features: {
-//                   'custom-properties': false,
-//                   'nesting-rules': true,
-//               },
-//           },
-//       ],
-//       ['postcss-px2rem', { remUnit: 75 }],
-//   ],
-// };
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}" // For Next.js App Router
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+    // "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
